80be806f3993e731cd4bcdfbdf6032484bc42498
prototype585 <prototype585@protonmail.com>
2025-12-22 03:22:48 +0100

    added user context menu type to member list

--- src/lib/MemberList.svelte
+++ src/lib/MemberList.svelte
@@ -19,14 +19,17 @@
     }
     members = await get_members(
       currentServer.value.id,
-      currentChannel.value.id
+      currentChannel.value.id,
     );
   });
 </script>
 
 <ul class="flex flex-col grow overflow-x-hidden gap-0.5 m-2">
   {#each members as member}
-    <li class="flex p-1 cursor-pointer hover:bg-white/5 rounded-xl">
+    <li
+      class="flex p-1 cursor-pointer hover:bg-white/5 rounded-xl"
+      data-ctx-type="user"
+    >
       <Avatar size="36" pic={member.picture} name={member.display_name} />
       <span class="ml-2 text-ellipsis whitespace-nowrap overflow-x-hidden"
         >{member.display_name}</span

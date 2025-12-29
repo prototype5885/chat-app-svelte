6216d11cc501977171327c820c140816eae26923
prototype585 <prototype585@protonmail.com>
2025-12-29 19:15:50 +0100

    made wrapper for fetch

--- src/scripts/httpActions.ts
+++ src/scripts/httpActions.ts
@@ -2,7 +2,52 @@ import * as m from "./models";
 import { errorToast } from "./toast.svelte";
 import * as z from "zod/mini";
 
-const JSON_HEADER = { "Content-Type": "application/json" };
+async function fetchWrapper(
+  endpoint: string,
+  options: RequestInit,
+): Promise<void>;
+
+async function fetchWrapper<T>(
+  endpoint: string,
+  options: RequestInit,
+  schema?: z.ZodMiniType<T>,
+): Promise<T>;
+
+async function fetchWrapper<T>(
+  endpoint: string,
+  options: RequestInit,
+  schema?: z.ZodMiniType<T> | undefined,
+): Promise<void | T> {
+  const headers = new Headers(options.headers);
+
+  if (options.body && !(options.body instanceof FormData)) {
+    headers.set("Content-Type", "application/json");
+  }
+
+  const response = await fetch(endpoint, {
+    ...options,
+    headers,
+  });
+
+  if (!response.ok) {
+    const errJson = await response.json();
+    errorToast(errJson.detail, response.status.toString());
+  }
+
+  if (!schema) {
+    return;
+  }
+
+  const json = await response.json();
+
+  const result = schema.safeParse(json);
+  if (!result.success) {
+    errorToast(result.error.message, result.error.name, false);
+    throw result.error;
+  }
+
+  return result.data;
+}
 
 export async function get_user_id() {
   const response = await fetch("/api/v1/user_id", { method: "GET" });
@@ -12,101 +57,69 @@ export async function get_user_id() {
       window.location.href = "./login.html";
       return;
     }
-    errorToast(response.statusText, response.statusText);
+    errorToast(response.statusText, response.status.toString());
   }
 
   return await response.text();
 }
 
 export async function get_user_info() {
-  const response = await fetch("/api/v1/user", { method: "GET" });
-
-  if (!response.ok) errorToast(response.statusText, response.statusText);
-
-  const result = m.UserSchema.safeParse(await response.json());
-  if (!result.success) errorToast(result.error.message, result.error.name);
-
-  return result.data!;
+  return await fetchWrapper("/api/v1/user", { method: "GET" }, m.UserSchema);
 }
 
-export async function update_user_info(
-  formData: FormData,
-): Promise<m.UpdateUserInfoModel> {
-  const response = await fetch("/api/v1/user", {
-    method: "PATCH",
-    body: formData,
-  });
-
-  if (!response.ok) errorToast(response.statusText, response.statusText);
-
-  const result = m.UpdateUserInfoSchema.safeParse(await response.json());
-  if (!result.success) errorToast(result.error.message, result.error.name);
-
-  return result.data!;
+export async function update_user_info(formData: FormData) {
+  return await fetchWrapper(
+    "/api/v1/user",
+    {
+      method: "PATCH",
+      body: formData,
+    },
+    m.UpdateUserInfoSchema,
+  );
 }
 
-export async function create_server(name: string): Promise<m.ServerModel> {
-  const response = await fetch("/api/v1/server", {
-    method: "POST",
-    headers: JSON_HEADER,
-    body: JSON.stringify({ name: name }),
-  });
-
-  if (!response.ok) errorToast(response.statusText, response.statusText);
-
-  const result = m.ServerSchema.safeParse(await response.json());
-  if (!result.success) errorToast(result.error.message, result.error.name);
-
-  return result.data!;
+export async function create_server(name: string) {
+  return await fetchWrapper(
+    "/api/v1/server",
+    {
+      method: "POST",
+      body: JSON.stringify({ name: name }),
+    },
+    m.ServerSchema,
+  );
 }
 
-export async function get_server_info(
-  serverID: string,
-): Promise<m.ServerModel> {
-  const response = await fetch(`/api/v1/server/${serverID}`, { method: "GET" });
-
-  if (!response.ok) errorToast(response.statusText, response.statusText);
-
-  const result = m.ServerSchema.safeParse(await response.json());
-  if (!result.success) errorToast(result.error.message, result.error.name);
-
-  return result.data!;
+export async function get_server_info(serverID: string) {
+  return await fetchWrapper(
+    `/api/v1/server/${serverID}`,
+    { method: "GET" },
+    m.ServerSchema,
+  );
 }
 
-export async function update_server_info(
-  formData: FormData,
-  serverID: string,
-): Promise<m.UpdateServerInfoModel> {
-  const response = await fetch(`/api/v1/server/${serverID}`, {
-    method: "PATCH",
-    body: formData,
-  });
-
-  if (!response.ok) errorToast(response.statusText, response.statusText);
-
-  const result = m.UpdateServerInfoSchema.safeParse(await response.json());
-  if (!result.success) errorToast(result.error.message, result.error.name);
-
-  return result.data!;
+export async function update_server_info(formData: FormData, serverID: string) {
+  return await fetchWrapper(
+    `/api/v1/server/${serverID}`,
+    {
+      method: "PATCH",
+      body: formData,
+    },
+    m.UpdateServerInfoSchema,
+  );
 }
 
-export async function get_servers(): Promise<m.ServerModel[]> {
-  const response = await fetch("/api/v1/servers", { method: "GET" });
-
-  if (!response.ok) errorToast(response.statusText, response.statusText);
-
-  const result = z.array(m.ServerSchema).safeParse(await response.json());
-  if (!result.success) errorToast(result.error.message, result.error.name);
-
-  return result.data!;
+export async function get_servers() {
+  return await fetchWrapper(
+    "/api/v1/servers",
+    { method: "GET" },
+    z.array(m.ServerSchema),
+  );
 }
 
 export async function delete_server(serverID: string) {
-  const response = await fetch(`/api/v1/server/${serverID}`, {
+  await fetchWrapper(`/api/v1/server/${serverID}`, {
     method: "DELETE",
   });
-
-  if (!response.ok) errorToast(response.statusText, response.statusText);
 
   // no need to store last channel ID of this server anymore
   localStorage.removeItem(serverID);
@@ -115,144 +128,100 @@ export async function delete_server(serverID: string) {
 }
 
 export async function create_channel(serverID: string, name: string) {
-  const response = await fetch(`/api/v1/server/${serverID}/channel`, {
+  await fetchWrapper(`/api/v1/server/${serverID}/channel`, {
     method: "POST",
-    headers: JSON_HEADER,
     body: JSON.stringify({ name: name }),
   });
-
-  if (!response.ok) errorToast(response.statusText, response.statusText);
 
   // socket.io response
 }
 
-export async function get_channel_info(
-  channelID: string,
-): Promise<m.ChannelModel> {
-  const response = await fetch(`/api/v1/channel/${channelID}`, {
-    method: "GET",
-  });
-
-  if (!response.ok) errorToast(response.statusText, response.statusText);
-
-  const result = m.ChannelSchema.safeParse(await response.json());
-  if (!result.success) errorToast(result.error.message, result.error.name);
-
-  return result.data!;
+export async function get_channel_info(channelID: string) {
+  return await fetchWrapper(
+    `/api/v1/channel/${channelID}`,
+    {
+      method: "GET",
+    },
+    m.ChannelSchema,
+  );
 }
 
 export async function update_channel_info(
   formData: FormData,
   channelID: string,
-): Promise<m.UpdateChannelInfoModel> {
-  const response = await fetch(`/api/v1/channel/${channelID}`, {
-    method: "PATCH",
-    body: formData,
-  });
-
-  if (!response.ok) errorToast(response.statusText, response.statusText);
-
-  const result = m.UpdateChannelInfoSchema.safeParse(await response.json());
-  if (!result.success) errorToast(result.error.message, result.error.name);
-
-  return result.data!;
-}
-
-export async function get_channels(
-  serverID: string,
-): Promise<m.ChannelModel[]> {
-  const response = await fetch(`/api/v1/server/${serverID}/channels`, {
-    method: "GET",
-  });
-
-  if (!response.ok) errorToast(response.statusText, response.statusText);
-
-  const result = z.array(m.ChannelSchema).safeParse(await response.json());
-  if (!result.success) errorToast(result.error.message, result.error.name);
-
-  return result.data!;
-}
-
-export async function delete_channel(channelID: string) {
-  const response = await fetch(`/api/v1/channel/${channelID}`, {
-    method: "DELETE",
-  });
-
-  if (!response.ok) errorToast(response.statusText, response.statusText);
+) {
+  return await fetchWrapper(
+    `/api/v1/channel/${channelID}`,
+    {
+      method: "PATCH",
+      body: formData,
+    },
+    m.UpdateChannelInfoSchema,
+  );
 
   // socket.io response
 }
 
-export async function get_members(
-  serverID: string,
-): Promise<m.UserDisplayModel[]> {
-  const response = await fetch(`/api/v1/server/${serverID}/members`, {
-    method: "GET",
+export async function get_channels(serverID: string) {
+  return await fetchWrapper(
+    `/api/v1/server/${serverID}/channels`,
+    { method: "GET" },
+    z.array(m.ChannelSchema),
+  );
+}
+
+export async function delete_channel(channelID: string) {
+  await fetchWrapper(`/api/v1/channel/${channelID}`, {
+    method: "DELETE",
   });
 
-  if (!response.ok) errorToast(response.statusText, response.statusText);
+  // socket.io response
+}
 
-  const result = z.array(m.UserDisplaySchema).safeParse(await response.json());
-  if (!result.success) errorToast(result.error.message, result.error.name);
-
-  return result.data!;
+export async function get_members(serverID: string) {
+  return await fetchWrapper(
+    `/api/v1/server/${serverID}/members`,
+    { method: "GET" },
+    z.array(m.UserDisplaySchema),
+  );
 }
 
 export async function create_message(channelID: string, message: string) {
-  const response = await fetch(`/api/v1/channel/${channelID}/message`, {
+  await fetchWrapper(`/api/v1/channel/${channelID}/message`, {
     method: "POST",
-    headers: JSON_HEADER,
     body: JSON.stringify({ message: message }),
   });
-
-  if (!response.ok) errorToast(response.statusText, response.statusText);
 
   // socket.io response
 }
 
 export async function edit_message(messageID: string, message: string) {
-  const response = await fetch(`/api/v1/message/${messageID}`, {
+  await fetchWrapper(`/api/v1/message/${messageID}`, {
     method: "PATCH",
-    headers: JSON_HEADER,
     body: JSON.stringify({ message: message }),
   });
-
-  if (!response.ok) errorToast(response.statusText, response.statusText);
 
   // socket.io response
 }
 
-export async function get_messages(
-  channelID: string,
-): Promise<m.MessageModel[]> {
-  const response = await fetch(`/api/v1/channel/${channelID}/messages`, {
-    method: "GET",
-  });
-
-  if (!response.ok) errorToast(response.statusText, response.statusText);
-
-  const result = z.array(m.MessageSchema).safeParse(await response.json());
-  if (!result.success) errorToast(result.error.message, result.error.name);
-
-  return result.data!;
+export async function get_messages(channelID: string) {
+  return await fetchWrapper(
+    `/api/v1/channel/${channelID}/messages`,
+    { method: "GET" },
+    z.array(m.MessageSchema),
+  );
 }
 
 export async function delete_message(messageID: string) {
-  const response = await fetch(`/api/v1/message/${messageID}`, {
-    method: "DELETE",
-  });
-
-  if (!response.ok) errorToast(response.statusText, response.statusText);
+  await fetchWrapper(`/api/v1/message/${messageID}`, { method: "DELETE" });
 
   // socket.io response
 }
 
 export async function typing(channelID: string, value: "start" | "stop") {
-  const response = await fetch(`/api/v1/channel/${channelID}/typing/${value}`, {
+  await fetchWrapper(`/api/v1/channel/${channelID}/typing/${value}`, {
     method: "POST",
   });
-
-  if (!response.ok) errorToast(response.statusText, response.statusText);
 
   // socket.io response
 }

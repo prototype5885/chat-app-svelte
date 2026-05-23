4fc198ed9ef2a56b8f8ac9363f618ebc09e8bdbb
prototype585 <prototype585@protonmail.com>
2026-05-23 18:31:41 +0200

    changed backend address to new default

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 
-const backendAddress = "http://127.0.0.1:8000";
+const backendAddress = "http://localhost:1848";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), svelte()],
   base: "/svelte",
   server: {
-    host: "127.0.0.1",
+    host: "localhost",
     port: 3000,
     proxy: {
       "/api": {

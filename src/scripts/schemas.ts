1e811c5e0286d1ec365ba2859d593bf3c0f743f8
prototype585 <prototype585@protonmail.com>
2026-07-08 00:30:03 +0200

    removed unused interfaces from schemas

--- src/scripts/schemas.ts
+++ src/scripts/schemas.ts
@@ -31,10 +31,6 @@ export interface UserSchema {
   online: boolean | null;
 }
 
-export interface UserEditRequest {
-  display_name: string | null;
-}
-
 export interface UserEditResponse {
   id: bigint;
   display_name?: string;
@@ -47,41 +43,10 @@ export interface UserOnline {
   online: boolean;
 }
 
-export interface ServerCreateRequest {
-  name: string;
-}
-
-export interface ServerEditRequest {
-  name: string | null;
-}
-
 export interface ChannelSchema {
   id: bigint;
   server_id: bigint;
   name: string;
-}
-
-export interface ChannelCreateRequest {
-  name: string;
-}
-
-export interface ChannelEditRequest {
-  name: string | null;
-}
-
-export interface MessageCreateRequest {
-  name: string | null;
-}
-
-export interface MessageEditRequest {
-  name: string;
-}
-
-export interface MessageEditResponse {
-  id: bigint;
-  message: string;
-  attachments: string | null;
-  edited: string | null;
 }
 
 export interface Attachment {
@@ -99,9 +64,4 @@ export interface MessageResponse {
   display_name: string;
   picture: string | null;
   attachments: Attachment[] | null;
-}
-
-export interface TypingSchema {
-  user_id: bigint;
-  display_name: string | null;
 }
